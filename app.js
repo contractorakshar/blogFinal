394727c5a6f851df5ece186dc95b6307da550f99
contractorakshar <contractorakshar8319@gmail.com>
2021-05-07 18:54:11 +0530

    Trial

--- app.js
+++ app.js
@@ -17,29 +17,6 @@ const { compare } = require("bcryptjs");
 // const Admin = require("./routes/AdminRoutes");
 // Initialize express app
 const app = express();
-app.get("/", (req, res) => {
-  let a = "happy";
-  // console.log("hello");
-  // res.send('Hello world');
-  fetch("https://aplet123-wordnet-search-v1.p.rapidapi.com/master?word=" + a, {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": "d00d745d82msh49a483b75ee89fdp14464ajsneb0658917638",
-      "x-rapidapi-host": "aplet123-wordnet-search-v1.p.rapidapi.com",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      let d = JSON.stringify(data.definition);
-      //   console.log(d.split(";"));
-      let arr = d.split(";");
-      // res.send(arr[0]);
-      console.log(arr[0]);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-});
 // Database Connection
 mongoose
   .connect(urls.mongoDB, {
