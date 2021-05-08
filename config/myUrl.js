755333ad2735ae4a2abb8651663babb01b270de8
contractorakshar <56545966+contractorakshar@users.noreply.github.com>
2021-05-08 11:47:44 +0530

    Delete config directory

--- config/myUrl.js
+++
@@ -1,5 +0,0 @@
-module.exports = myUrl = {
-  mongoDB: "mongodb://localhost:27017/blogTrial",
-  // mongoBD:
-  //   "mongodb+srv://Akshar:akshar@2021@cluster0.begp6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-};
